e113a93fb69cb0298b2c2b7a73acccabf8fb4ef9
Muksud Mahi <mmahi1@my.centennialcollege.ca>
2021-05-22 21:57:48 -0400

    Correct welcome message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello this is Muksud');
+  res.end('Hello this is Muksud Hussain Mahi\nID:301155894');
 });
 
 server.listen(port, hostname, () => {
